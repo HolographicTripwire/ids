cf3ba508a7624f403c6256c141314b1f1f4e1b88
HolographicTripwire <josephsmagen@gmail.com>
2025-05-06 20:17:36 +0100

    Make all IdImpls use .0 instead of .val

--- src/implementations/by_size.rs
+++ src/implementations/by_size.rs
@@ -5,21 +5,21 @@ use crate::base::IdImpl;
 const TRY_FROM_ERR: &str = "The err branch of an IdImpl's TryFrom was executed. The rust analyser claims that this result is impossible.";
 
 /// An 8-bit [IdImpl]
-#[derive(Clone, Copy, PartialEq, Eq, Hash)]
-pub struct Id8 { val: u8 }
+#[derive(Clone, Copy, PartialEq, Eq, Debug, Hash)]
+pub struct Id8(u8);
 impl IdImpl for Id8 { 
-    fn first() -> Self { Self { val: 0 } }
+    fn first() -> Self { Self(0) }
     fn next(&self) -> Result<Self,()> {
-        if self.val == u8::MAX || Ok(self.val) == usize::MAX.try_into()
+        if self.0 == u8::MAX || Ok(self.0) == usize::MAX.try_into()
             { Err(()) }
-        else { Ok(Self { val: self.val + 1 }) }
+        else { Ok(Self(self.0 + 1)) }
     }
 }
 impl TryFrom<usize> for Id8 {
     type Error = TryFromIntError;
     fn try_from(value: usize) -> Result<Self, Self::Error> {
         match u8::try_from(value) {
-            Ok(val) => Ok(Self { val }),
+            Ok(val) => Ok(Self(val)),
             Err(err) => Err(err),
         }
     }
@@ -27,7 +27,7 @@ impl TryFrom<usize> for Id8 {
 impl TryFrom<Id8> for usize {
     type Error = TryFromIntError;
     fn try_from(value: Id8) -> Result<Self, Self::Error> {
-        match usize::try_from(value.val) {
+        match usize::try_from(value.0) {
             Ok(val) => Ok(val),
             Err(_e) => { panic!("{}", TRY_FROM_ERR) },
         }
@@ -35,21 +35,21 @@ impl TryFrom<Id8> for usize {
 }
 
 /// A 16-bit [IdImpl]
-#[derive(Clone, Copy, PartialEq, Eq, Hash)]
-pub struct Id16 { val: u16 }
+#[derive(Clone, Copy, PartialEq, Eq, Debug, Hash)]
+pub struct Id16(u16);
 impl IdImpl for Id16 { 
-    fn first() -> Self { Self { val: 0 } }
+    fn first() -> Self { Self(0) }
     fn next(&self) -> Result<Self,()> {
-        if self.val == u16::MAX || Ok(self.val) == usize::MAX.try_into()
+        if self.0 == u16::MAX || Ok(self.0) == usize::MAX.try_into()
             { Err(()) }
-        else { Ok(Self { val: self.val + 1 }) }
+        else { Ok(Self(self.0 + 1)) }
     }
 }
 impl TryFrom<usize> for Id16 {
     type Error = TryFromIntError;
     fn try_from(value: usize) -> Result<Self, Self::Error> {
         match u16::try_from(value) {
-            Ok(val) => Ok(Self { val }),
+            Ok(val) => Ok(Self(val)),
             Err(err) => Err(err),
         }
     }
@@ -57,7 +57,7 @@ impl TryFrom<usize> for Id16 {
 impl TryFrom<Id16> for usize {
     type Error = TryFromIntError;
     fn try_from(value: Id16) -> Result<Self, Self::Error> {
-        match usize::try_from(value.val) {
+        match usize::try_from(value.0) {
             Ok(val) => Ok(val),
             Err(_e) => { panic!("{}", TRY_FROM_ERR) },
         }
@@ -65,21 +65,21 @@ impl TryFrom<Id16> for usize {
 }
 
 /// A 32-bit [IdImpl]
-#[derive(Clone, Copy, PartialEq, Eq, Hash)]
-pub struct Id32 { val: u32 }
+#[derive(Clone, Copy, PartialEq, Eq, Debug, Hash)]
+pub struct Id32(u32);
 impl IdImpl for Id32 { 
-    fn first() -> Self { Self { val: 0 } }
+    fn first() -> Self { Self(0) }
     fn next(&self) -> Result<Self,()> {
-        if self.val == u32::MAX || Ok(self.val) == usize::MAX.try_into()
+        if self.0 == u32::MAX || Ok(self.0) == usize::MAX.try_into()
             { Err(()) }
-        else { Ok(Self { val: self.val + 1 }) }
+        else { Ok(Self(self.0 + 1)) }
     }
 }
 impl TryFrom<usize> for Id32 {
     type Error = TryFromIntError;
     fn try_from(value: usize) -> Result<Self, Self::Error> {
         match u32::try_from(value) {
-            Ok(val) => Ok(Self { val }),
+            Ok(val) => Ok(Self(val)),
             Err(err) => Err(err),
         }
     }
@@ -87,7 +87,7 @@ impl TryFrom<usize> for Id32 {
 impl TryFrom<Id32> for usize {
     type Error = TryFromIntError;
     fn try_from(value: Id32) -> Result<Self, Self::Error> {
-        match usize::try_from(value.val) {
+        match usize::try_from(value.0) {
             Ok(val) => Ok(val),
             Err(_e) => { panic!("{}", TRY_FROM_ERR) },
         }
@@ -95,21 +95,21 @@ impl TryFrom<Id32> for usize {
 }
 
 /// An 64-bit [IdImpl]
-#[derive(Clone, Copy, PartialEq, Eq, Hash)]
-pub struct Id64 { val: u64 }
+#[derive(Clone, Copy, PartialEq, Eq, Debug, Hash)]
+pub struct Id64(u64);
 impl IdImpl for Id64 { 
-    fn first() -> Self { Self { val: 0 } }
+    fn first() -> Self { Self(0) }
     fn next(&self) -> Result<Self,()> {
-        if self.val == u64::MAX || Ok(self.val) == usize::MAX.try_into()
+        if self.0 == u64::MAX || Ok(self.0) == usize::MAX.try_into()
             { Err(()) }
-        else { Ok(Self { val: self.val + 1 }) }
+        else { Ok(Self(self.0 + 1)) }
     }
 }
 impl TryFrom<usize> for Id64 {
     type Error = TryFromIntError;
     fn try_from(value: usize) -> Result<Self, Self::Error> {
         match u64::try_from(value) {
-            Ok(val) => Ok(Self { val }),
+            Ok(val) => Ok(Self(val)),
             Err(err) => Err(err),
         }
     }
@@ -117,7 +117,7 @@ impl TryFrom<usize> for Id64 {
 impl TryFrom<Id64> for usize {
     type Error = TryFromIntError;
     fn try_from(value: Id64) -> Result<Self, Self::Error> {
-        match usize::try_from(value.val) {
+        match usize::try_from(value.0) {
             Ok(val) => Ok(val),
             Err(_e) => { panic!("{}", TRY_FROM_ERR) },
         }
@@ -125,21 +125,21 @@ impl TryFrom<Id64> for usize {
 }
 
 /// An 128-bit [IdImpl]
-#[derive(Clone, Copy, PartialEq, Eq, Hash)]
-pub struct Id128 { val: u128 }
-impl IdImpl for Id128 { 
-    fn first() -> Self { Self { val: 0 } }
+#[derive(Clone, Copy, PartialEq, Eq, Debug, Hash)]
+pub struct Id128(u128);
+impl IdImpl for Id128 {
+    fn first() -> Self { Self(0) }
     fn next(&self) -> Result<Self,()> {
-        if self.val == u128::MAX || Ok(self.val) == usize::MAX.try_into()
+        if self.0 == u128::MAX || Ok(self.0) == usize::MAX.try_into()
             { Err(()) }
-        else { Ok(Self { val: self.val + 1 }) }
+        else { Ok(Self(self.0 + 1)) }
     }
 }
 impl TryFrom<usize> for Id128 {
     type Error = TryFromIntError;
     fn try_from(value: usize) -> Result<Self, Self::Error> {
         match u128::try_from(value) {
-            Ok(val) => Ok(Self { val }),
+            Ok(val) => Ok(Self(val)),
             Err(err) => Err(err),
         }
     }
@@ -147,7 +147,7 @@ impl TryFrom<usize> for Id128 {
 impl TryFrom<Id128> for usize {
     type Error = TryFromIntError;
     fn try_from(value: Id128) -> Result<Self, Self::Error> {
-        match usize::try_from(value.val) {
+        match usize::try_from(value.0) {
             Ok(val) => Ok(val),
             Err(_e) => { panic!("{}", TRY_FROM_ERR) },
         }
